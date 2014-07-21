e0f190dbb8ac455e7bf8442a8cc6e8b3984d361d
Daniel Engel <daniel.engel@gmail.com>
2014-07-20 22:05:28 -0400

    Checkpoint

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/', function(request, response) {
 });
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.set('ipaddr', ip)
+app.set('port', port)
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
